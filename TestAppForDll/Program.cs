a4bb356129158bf024498bc954c6e891b2b8822e
Andrew Thomas <andrew@thomas.net.nz>
2022-11-29 08:43:46 +1300

    Started work

---
+++ TestAppForDll/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.InteropServices;
+
+
+namespace DronePort.TelemetryLogger;
+
+internal class Program
+{
+  private const string DllFilePath = @"mavsdk.dll";
+
+  static async Task Main(string[] args)
+  {
+
+
+  }
+
+  [DllImport(DllFilePath, CallingConvention = CallingConvention.Cdecl)]
+  private extern static int test(int number);
+
+  public static int Test(int number)
+  {
+    return test(number);
+  }
+}
